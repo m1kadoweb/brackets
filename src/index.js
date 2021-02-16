98a6aaa057dc7d2af4f30153a3cb7c6ec806de3e
m1kadoweb <37939676+m1kadoweb@users.noreply.github.com>
2021-02-16 10:03:31 +0500

    task complete

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-    let arr = []
-    let brackets = []
+    let arr = [];
+    let brackets = [];
+    let step = 0;
     
-    for(let k = 0; k === 0;) {
-        k = 1
-      
+    while(step == 0) {
+        step = 1;
+        
         for(let i = 0; i < bracketsConfig.length; i++) {
-        brackets[i] = bracketsConfig[i][0] + bracketsConfig[i][1]         
-        arr = str.split(brackets[i])
-        if(arr.length > 1) {
-          k = 0
+            brackets[i] = bracketsConfig[i][0] + bracketsConfig[i][1];
+            arr = str.split(brackets[i]);
+            console.log(arr)
+            if(arr.length > 1) {
+                step = 0;
+            }
+            str = arr.join('');
         }
-        str = arr.join('')        
-      }
-      
-      }
-      
-      return str === '' ? true : false
+    }
+    return str === '' ? true : false
 }
